920fd09a30aabd239389df6189136ed99d1140c7
dagolinuxoid <daniel1kdago@gmail.com>
2019-05-21 22:23:03 +0300

    longest increasing subsequence
    awesome commit ;)
    
    It looks like a working solution
    I didn't test it thoroughly yet and
    as you can se the current solution is pretty garbo.

--- other_algs/longest_increasing_subsequence.js
+++ other_algs/longest_increasing_subsequence.js
@@ -11,4 +11,63 @@ the array [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15],
 the longest increasing subsequence has length 6: it is 0, 2, 6, 9, 11, 15.
 */
 
+function getSequence(arr) {
+  let seq=[];
+  let max=0;
+  let pos=0;
+  for (let i=0; i<arr.length-1; i++) {
+    let sub=arr.slice(i+1);
+    let len=sub.filter(e=>e>=arr[i]).length;
+    if (max<len) {
+      max=len;
+      pos=i;
+    } else if (max===len) {
+      if (arr[i]<arr[pos])
+      {
+        max=len;
+        pos=i;
+      }
+    }
+  }
+  seq.push(arr[pos]);
+
+  arr=arr.slice(pos+1);
+
+  for (let i=0; i<arr.length; i++) {
+
+    if (arr[i]<seq[seq.length-1]) continue;
+
+    pos=i;
+    max=0;
+
+    for (let j=i; j<arr.length; j++) {
+
+      if (arr[j]>=seq[seq.length-1]) {
+        let len=countAbove(arr.slice(j));
+
+        if (max<len) {max=len; pos=j;}
+        else if (max===len) {
+          if (arr[pos]>arr[j]) {pos=j;}
+        }
+      }
+
+    }
+
+    i=pos;
+    seq.push(arr[pos]);
+  }
+
+  return seq.length;
+}
+
+function countAbove(arr) {
+  let first=arr[0];
+  return arr.slice(1).filter(e=>e>=first).length;
+}
+
+const assert=require('assert').strict;
+assert.equal(getSequence([0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15]),[0,2,6,9,11,15].length);
+assert.equal(getSequence([1,5,4,8,42]),4);
+assert.equal(getSequence([8,5,7,8,2]),3);
+assert.equal(getSequence([8,5,1,8,2]),2);
 
