fd4eb3663dfbf27302d6bc621003505f680a5ef2
dagolinuxoid <daniel1kdago@gmail.com>
2019-05-23 17:41:48 +0300

    longest_increasing_subsequence refactoring

--- other_algs/longest_increasing_subsequence.js
+++ other_algs/longest_increasing_subsequence.js
@@ -12,21 +12,15 @@ the longest increasing subsequence has length 6: it is 0, 2, 6, 9, 11, 15.
 */
 
 function getSequence(arr) {
-  let seq=[];
+  const seq=[];
   let max=0;
   let pos=0;
   for (let i=0; i<arr.length-1; i++) {
-    let sub=arr.slice(i+1);
-    let len=sub.filter(e=>e>=arr[i]).length;
-    if (max<len) {
+    const sub=arr.slice(i+1);
+    const len=sub.filter(e=>e>=arr[i]).length;
+    if (max<len || max===len && arr[i]<arr[pos]) {
       max=len;
       pos=i;
-    } else if (max===len) {
-      if (arr[i]<arr[pos])
-      {
-        max=len;
-        pos=i;
-      }
     }
   }
   seq.push(arr[pos]);
@@ -43,20 +37,19 @@ function getSequence(arr) {
     for (let j=i; j<arr.length; j++) {
 
       if (arr[j]>=seq[seq.length-1]) {
-        let len=countAbove(arr.slice(j));
+        const len=countAbove(arr.slice(j));
 
-        if (max<len) {max=len; pos=j;}
-        else if (max===len) {
-          if (arr[pos]>arr[j]) {pos=j;}
+        if (max<len || max===len && arr[pos]>arr[j]) {
+          max=len;
+          pos=j;
         }
       }
-
     }
 
     i=pos;
     seq.push(arr[pos]);
   }
-
+  console.log(seq);
   return seq.length;
 }
 
@@ -70,4 +63,5 @@ assert.equal(getSequence([0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15]),[0,2,6,9,11,15
 assert.equal(getSequence([1,5,4,8,42]),4);
 assert.equal(getSequence([8,5,7,8,2]),3);
 assert.equal(getSequence([8,5,1,8,2]),2);
+assert.equal(getSequence([7,2,1,3,8,4,9,1,2,6,5,9,3,8,1]),5);
 
